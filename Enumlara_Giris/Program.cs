da20369791fc3c07bb300c04b2cd41de75f8891f
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-05 19:05:44 +0300

    Add project files.

---
+++ Enumlara_Giris/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumlara_Giris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gunler gun = Gunler.pazartesi;
+
+            if(gun == Gunler.pazartesi)
+            {
+                Console.WriteLine("Gün Pazartesidir");
+            }
+            else if(gun == Gunler.salı)
+            {
+                Console.WriteLine("Gün Salıdır.");
+            }
+            else if(gun == Gunler.çarşamba)
+            {
+                Console.WriteLine("Gün Çarşambadır.");
+            }
+            else if(gun == Gunler.perşembe)
+            {
+                Console.WriteLine("Gün Perşembedir.");
+            }
+            else if(gun == Gunler.cuma)
+            {
+                 Console.WriteLine("Gün Cumadır");
+            }
+            else if (gun == Gunler.cumartesi)
+            {
+                Console.WriteLine("Gün Cumartesidir");
+            }
+            else if (gun == Gunler.pazar)
+            {
+                Console.WriteLine("Gün Pazartesidir");
+            }
+        }
+    }
+}
